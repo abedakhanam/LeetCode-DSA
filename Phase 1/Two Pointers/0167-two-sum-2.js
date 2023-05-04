176b8071a7fa7c05bf4ec18fc1d31b030a057a31
Abeda Khanam <abeda2152@gmail.com>
2023-05-04 22:09:45 +0600

    inital commit

---
+++ Phase 1/Two Pointers/0167-two-sum-2.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    //define pointers
+    let L = 0, R = numbers.length-1;
+    while(L < R){
+        let sum = numbers[L] + numbers[R];
+        if(sum == target){
+            return [L+1, R+1];
+        }else if(sum<target){
+            L++;
+        }else{
+            R--;
+        }
+    }
+    return [-1, -1];
+};
---
+++ Phase 1/Two Pointers/0283-move-zeroes.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let n = nums.length;
+  if (n < 2) {
+    return;
+  }
+  let L = 0,
+    R = 1;
+  while (R < n) {
+    if (nums[L] != 0) {
+      L++;
+      R++;
+    } else if (nums[R] == 0) {
+      R++;
+    } else {
+      let temp = nums[R];
+      nums[R] = nums[L];
+      nums[L] = temp;
+    }
+  }
+};
